f38111e4a81e5ca94b562f759847be9a89c9194e
Duncan Harris <duncan@sensiblecode.io>
2018-11-26 14:21:38 +0000

    Improve error handling
    Change to CSV format

--- pdftables.php
+++ pdftables.php
@@ -1,18 +1,18 @@
 <?php
 $c = curl_init();
 $cfile = curl_file_create('test.pdf', 'application/pdf');
-
-curl_setopt($c, CURLOPT_URL, '{{ .FullURL }}/api?key={{ .DisplayAPIKey }}&format=xml');
+$apikey = 'YOUR_API_KEY'; // from https://pdftables.com/api
+curl_setopt($c, CURLOPT_URL, "https://pdftables.com/api?key=$apikey&format=csv");
 curl_setopt($c, CURLOPT_POSTFIELDS, array('file' => $cfile));
 curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c, CURLOPT_FAILONERROR,true);
 curl_setopt($c, CURLOPT_ENCODING, "gzip,deflate");
-
 $result = curl_exec($c);
-if (curl_errno($c)) {
-    print('Error calling PDFTables: ' . curl_error($c));
+if (curl_errno($c) > 0) {
+    print('Error calling PDFTables: '.curl_error($c).PHP_EOL);
+} else {
+  // save the CSV we got from PDFTables to a file
+  file_put_contents ("test.csv", $result);
 }
-
-// save the XML we got from PDFTables to a file
-file_put_contents ("test.xml", $result);
-
 curl_close($c);
+?>
