da1d351c7fac13b59912387c13fdda6ec7c1a01f
Duncan Harris <duncan@sensiblecode.io>
2019-04-30 16:40:55 +0100

    Add PDF readability check
    Add PDF file readability check.
    
    Also space out the code for legibility.

--- pdftables.php
+++ pdftables.php
@@ -1,18 +1,32 @@
 <?php
+
+$pdf_file = 'test.pdf';
+
+if (!is_readable($pdf_file)) {
+        print("Error: file does not exist or is not readable: $pdf_file\n");
+        return;
+}
+
 $c = curl_init();
-$cfile = curl_file_create('test.pdf', 'application/pdf');
+
+$cfile = curl_file_create($pdf_file, 'application/pdf');
+
 $apikey = 'YOUR_API_KEY'; // from https://pdftables.com/api
 curl_setopt($c, CURLOPT_URL, "https://pdftables.com/api?key=$apikey&format=csv");
 curl_setopt($c, CURLOPT_POSTFIELDS, array('file' => $cfile));
 curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($c, CURLOPT_FAILONERROR,true);
 curl_setopt($c, CURLOPT_ENCODING, "gzip,deflate");
+
 $result = curl_exec($c);
+
 if (curl_errno($c) > 0) {
     print('Error calling PDFTables: '.curl_error($c).PHP_EOL);
 } else {
   // save the CSV we got from PDFTables to a file
-  file_put_contents ("test.csv", $result);
+  file_put_contents ($pdf_file . ".csv", $result);
 }
+
 curl_close($c);
+
 ?>
